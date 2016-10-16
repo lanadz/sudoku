6bf325bbba4041bfe9c1eb575b4b1871c5340784
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-10-16 14:43:01 +0800

    Add board getters for row, cell and box based on cell's index

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -24,4 +24,31 @@ RSpec.describe Sudoku::Board do
 
     expect(board_values).to match_array input_matrix
   end
+
+  describe '#get_row_by' do
+    let(:expected_row_values) { [2, 0, 8, 0, 7, 0, 9, 0, 1] }
+
+    it 'gets row based on cell\'s index' do
+      row = subject.get_row_by(cell: Sudoku::Cell.new(x: 2, y: 1, value: 0))
+      expect(row.map(&:value)).to eq expected_row_values
+    end
+  end
+
+  describe '#get_col_by' do
+    let(:expected_col_values) { [0, 7, 0, 2, 8, 3, 0, 6, 0] }
+
+    it 'gets col based on cell\'s index' do
+      col = subject.get_col_by(cell: Sudoku::Cell.new(x: 2, y: 1, value: 0))
+      expect(col.map(&:value)).to eq expected_col_values
+    end
+  end
+
+  describe '#get_box_by' do
+    let(:expected_box_values) { [0, 1, 0, 6, 3, 5, 0, 4, 0] }
+
+    it 'gets box based on cell\'s index' do
+      box = subject.get_box_by(cell: Sudoku::Cell.new(x: 4, y: 5, value: 0))
+      expect(box.map(&:value)).to eq expected_box_values
+    end
+  end
 end
--- sudoku/board.rb
+++ sudoku/board.rb
@@ -13,5 +13,45 @@ module Sudoku
         @fields << row
       end
     end
+
+    def get_row_by(cell:)
+      fields[cell.x]
+    end
+
+    def get_col_by(cell:)
+      (0...9).map do |x|
+        fields[x][cell.y]
+      end
+    end
+
+    def get_box_by(cell:)
+      top_left_x_of_box =containing_box_start_index(cell.x)
+      top_left_y_of_box =containing_box_start_index(cell.y)
+
+      final_x = top_left_x_of_box + 3
+      final_y = top_left_y_of_box + 3
+      box = []
+
+      (top_left_x_of_box...final_x).each do |x|
+        (top_left_y_of_box...final_y).each do |y|
+          box << fields[x][y]
+        end
+      end
+
+      box
+    end
+
+    private
+
+    def containing_box_start_index(index)
+      case index
+        when (0..2) then
+          0
+        when (3..5) then
+          3
+        when (6..8) then
+          6
+      end
+    end
   end
 end
